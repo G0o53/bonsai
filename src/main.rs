2460db00eb285b4775eb87c3a3384b523135992e
MatthewLizzard <161202393+MatthewLizzard@users.noreply.github.com>
2026-07-10 11:34:18 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::io::{self, Write};
+
+fn main() {
+    let mut stderr = io::stderr();
+    let mut stdout = io::stdout();
+    let args: Vec<String> = env::args().collect();
+
+    let hook = &args[1];
+
+    if hook == "echo" {
+        let _ = write!(stderr, "\n▙ "); 
+        let _ = stderr.flush();
+        let arg = &args[2];
+        let _ = write!(stdout, "{arg}\n");
+        let _ = stdout.flush();
+    } else if hook == "cd" {
+        let _ = write!(stderr, "\n▚ \n"); 
+        let _ = stderr.flush();
+        let arg = &args[2];
+        let _ = write!(stdout, "{arg}\n");
+     } else if hook == "exit" {
+        let exit_code: i32 = args[2].parse().unwrap();
+        let _ = write!(stderr, "\n█\n"); 
+        let _ = stderr.flush(); 
+        std::process::exit(exit_code);
+    } else if hook == "pwd" {
+        let _ = write!(stderr, "\n▞ "); 
+        let _ = stderr.flush();
+        let path = env::current_dir().expect("Failed to get current directory");
+        let _ = write!(stdout, "{}\n", path.display());
+        let _ = stdout.flush();
+    } else if hook == "read" {
+        let _ = write!(stderr, "\n▌ "); 
+        let _ = stderr.flush();
+        let mut input = String::new();
+        
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                std::process::exit(1);
+            }
+        }
+        let _ = write!(stdout, "{}", input.trim_end());
+        let _ = stdout.flush();
+        let _ = write!(stderr, "\n");
+        let _ = stderr.flush();
+    }
+}
+
---
+++ test.sh
@@ -0,0 +1,8 @@
+hook builtins ~/bonsai/target/release/bonsai
+
+echo "foo"
+cd /bin
+pwd
+read $VAR
+export e=E
+exit 0
